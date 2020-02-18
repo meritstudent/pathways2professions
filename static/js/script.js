ae4bea1f3ce93a968b55d6efb65ae1c0aca44cbb
Merit Student <student@meac.local>
2020-02-18 14:52:21 -0700

    map zoom?

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,38 @@
+// Hammer js jQuery plugin: https://github.com/hammerjs/jquery.hammer.js
+(function(factory) {
+  if (typeof define === 'function' && define.amd) {
+      define(['jquery', 'hammerjs'], factory);
+  } else if (typeof exports === 'object') {
+      factory(require('jquery'), require('hammerjs'));
+  } else {
+      factory(jQuery, Hammer);
+  }
+}(function($, Hammer) {
+  function hammerify(el, options) {
+      var $el = $(el);
+      if(!$el.data("hammer")) {
+          $el.data("hammer", new Hammer($el[0], options));
+      }
+  }
+
+  $.fn.hammer = function(options) {
+      return this.each(function() {
+          hammerify(this, options);
+      });
+  };
+
+  // extend the emit method to also trigger jQuery events
+  Hammer.Manager.prototype.emit = (function(originalEmit) {
+      return function(type, data) {
+          originalEmit.call(this, type, data);
+          $(this.element).trigger({
+              type: type,
+              gesture: data
+          });
+      };
+  })(Hammer.Manager.prototype.emit);
+}));
+
 var TxtRotate = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
@@ -53,4 +88,41 @@ var TxtRotate = function(el, toRotate, period) {
     css.type = "text/css";
     css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
     document.body.appendChild(css);
+
+    
+    // Click map
+    $("#map").click(
+      function(){console.log("Hello")}
+    )
+    var el = document.querySelector(".pinch");
+    var ham = new Hammer( el, {
+      domEvents: true
+    } );
+    var width = 1900;
+    var height = 400;
+    var left = 950;
+    var top = 220;
+
+    ham.get('pinch').set({ enable: true });
+
+    ham.on( "pinch", function( e ) {
+      console.log( "pinch" );
+      if ( width * e.scale >= 300 ) {
+        var img = el.childNodes[1];
+        img.style.width = (width * e.scale) + 'px';
+        img.style.marginLeft = (-left * e.scale) + 'px';
+        img.style.height = (height * e.scale) + 'px';
+        img.style.marginTop = (-top * e.scale) + 'px';
+       }
+       console.log( e.scale );
+    } );
+
+    ham.on( "pinchend", function( e ) {
+      width = width * e.scale;
+      height = height * e.scale;
+      left = left * e.scale;
+      top = top * e.scale;
+      console.log( width );
+    } );
+
   };
